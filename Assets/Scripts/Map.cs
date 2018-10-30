45087028b7793df4c7f9ab92e41f0ceb074adc43
AlexJanson <alex@hoofddorp.nl>
2018-10-30 08:36:18 +0100

    added tilemap

--- Assets/Scripts/Map.cs
+++ Assets/Scripts/Map.cs
@@ -4,36 +4,5 @@ using UnityEngine;
 
 public class Map : MonoBehaviour {
 
-    private GameObject[,] gridMap;
-
-    public int rows = 10;
-    public int cols = 10;
-
-    public Sprite tileSprite;
-
-    public GameObject prefab;
-    public int scaleSize;
-    Camera camera;
-
-
-    private void Start()
-    {
-        camera = FindObjectOfType<Camera>();
-        Vector3 topRightCorner = camera.ViewportToScreenPoint(new Vector3(0, 1, -1));
-        Vector3 topLeftCorner = camera.ViewportToScreenPoint(new Vector3(0, 1, camera.nearClipPlane));
-        print(topRightCorner);
-        print(topLeftCorner);
-
-        //transform.position = new Vector3(topRightCorner.x, topRightCorner.y, 1);
-        transform.position = new Vector3(topLeftCorner.x, topLeftCorner.y, 1);
-
-        gridMap = new GameObject[rows, cols];
-        for(int x = 0; x < rows; x++) {
-            for(int y = 0; y < cols; y++) {
-                gridMap[x, y] = Instantiate(prefab, transform);
-                gridMap[x, y].GetComponent<SpriteRenderer>().sprite = tileSprite;
-                gridMap[x, y].transform.localScale = new Vector3(scaleSize, scaleSize, 1);
-            }
-        }
-    }
+    
 }
