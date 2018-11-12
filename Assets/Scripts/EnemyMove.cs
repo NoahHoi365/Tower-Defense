37e196c0247d3cf2755b0c8e1743b59bc0a0e76e
NoahHoi365 <24922@ma-web.nl>
2018-11-12 15:41:14 +0100

    added ui

--- Assets/Scripts/EnemyMove.cs
+++ Assets/Scripts/EnemyMove.cs
@@ -1,12 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class EnemyMove : MonoBehaviour {
 
     public float speed = 5f;
     public float waitTime = .3f;
     public float turnSpeed = 90f;
+
+    public delegate void ReachedEndOfPath();
+    public event ReachedEndOfPath EndOfPathEvent;
 
     public Transform pathHolder;
 
@@ -41,6 +45,13 @@ public class EnemyMove : MonoBehaviour {
             }
             yield return null;
         }
+
+        print("reached end path");
+        if(EndOfPathEvent != null)
+        {
+            EndOfPathEvent();
+        }
+
     }
 
     IEnumerator TurnToFace(Vector3 lookTarget)
---
+++ Assets/Scripts/Lives.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lives : MonoBehaviour {
+
+    public int lives = 100;
+    EnemyMove enemyMove;
+
+	// Use this for initialization
+	void Start () {
+        enemyMove = FindObjectOfType<EnemyMove>();
+        enemyMove.EndOfPathEvent += LifeDecrease;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        print(lives);
+	}
+
+    void LifeDecrease()
+    {
+        lives--;
+    }
+}
