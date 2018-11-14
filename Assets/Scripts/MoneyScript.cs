026e49533c376983c60b5751fb966e9fa1a730bf
NoahHoi365 <24922@ma-web.nl>
2018-11-14 14:30:47 +0100

    UI script

---
+++ Assets/Scripts/MoneyScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class MoneyScript : MonoBehaviour {
+
+    public TextMeshProUGUI text;
+        
+	// Use this for initialization
+	void Start () {
+        text.text = "7666";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
